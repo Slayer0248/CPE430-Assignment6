775669cb7763f5db441b4509fabb33a1dd5b0be5
Clay Jacobs <clayjacobs245@gmail.com>
2016-02-27 12:27:09 -0800

    Binop methods

--- Asgn6.cs
+++ Asgn6.cs
@@ -2,7 +2,30 @@ using System;
 using System.Collections;
 
 struct OWWQ3{
-   public int numBinopHandle(String op, int num1, int num2) {
+   
+   
+   
+}
+
+class LangException : Exception
+{
+    private String eMessage;
+
+    public LangException(string message)
+    {
+       eMessage = message;
+    }
+    
+    public String getMessage() {
+       return eMessage;
+    }
+
+}
+
+
+class OWWQ3Runner
+{
+   public double numBinopHandle(String op, double num1, double num2) {
       if (op.Equals("+", StringComparison.Ordinal)) {
          return num1 + num2;
       }
@@ -14,7 +37,8 @@ struct OWWQ3{
       }
       else if (op.Equals("/", StringComparison.Ordinal)) {
          if (num2 == 0) {
-         
+            LangException ex = new LangException("Divide by zero exception");
+            throw ex;
          }
          else {
             return num1 / num2;
@@ -22,17 +46,63 @@ struct OWWQ3{
          
       }
       else {
-      
+         LangException ex = new LangException("Bad num binop");
+         throw ex;
       }
    }
    
-   public boolean booleanBinopHandle(String op, int num1, int num2) {
-}
-
-
-class OWWQ3Runner
-{
-   
+   public bool booleanBinopHandle(String op, Object prim1, Object prim2) {
+      Type prim1Type = prim1.GetType();
+      Type prim2Type = prim2.GetType();
+      if (op.Equals("<=", StringComparison.Ordinal)) {
+         if (prim1Type.Equals(typeof(double)) && prim2Type.Equals(typeof(double))) {
+            double num1 = (double)prim1;
+            double num2 = (double)prim2;
+            if (num1 <= num2) { 
+               return true;
+            }
+            else {
+               return false;
+            }
+         }
+         else {
+            LangException ex = new LangException("Both types for <= must be doubles");
+            throw ex;
+         }
+      }
+      else if (op.Equals("eq?", StringComparison.Ordinal)) {
+         if (!prim1Type.Equals(prim2Type)) {
+            return false;
+         }
+         else if (prim1Type.Equals(typeof(double))) {
+            double num1 = (double)prim1;
+            double num2 = (double)prim2;
+            
+            if (num1 == num2) {
+               return true;
+            }
+            else {
+               return false;
+            }
+         }
+         else {
+            bool bool1 = (bool)prim1;
+            bool bool2 = (bool)prim2;
+            
+            if (bool1 == bool2) {
+               return true;
+            }
+            else {
+               return false;
+            }
+         }
+         
+      }
+      else {
+         LangException ex = new LangException("Bad boolean binop");
+         throw ex;
+      }
+   }
 
    static void Main(string[] args) {
       
--- hello.cs
+++ hello.cs
@@ -1,11 +1,52 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Windows.Forms;
+
+class LangException : Exception
+{
+    private String eMessage;
+
+    public LangException(string message)
+    {
+       eMessage = message;
+    }
+    
+    public String getMessage() {
+       return eMessage;
+    }
+
+}
+
+
+
 class Hello
 {
+   static void testException() {
+      throw new CustomException("exception success");
+   }
+
    static void Main() {
       
-      MessageBox.Show("Some text", "Some title", 
-             MessageBoxButtons.OK, MessageBoxIcon.Error);
-      Console.WriteLine("hello, world");
+      try
+      {
+         testException();
+         Console.WriteLine("hello, world");
+      }
+      catch (CustomException ex)
+      {
+          System.Console.WriteLine(ex.ToString());
+          System.Console.WriteLine(ex.getMessage());
+      } 
+      //DialogResult result = MessageBox.Show("Some text", "Some title", 
+      //       MessageBoxButtons.OK, MessageBoxIcon.Error);
+      
    }
+   
+   
 }
